976ca172d150c5875bfe20ae2dd4602714d52475
kirpichev228 <kirpichev228@mail.ru>
2023-05-04 16:28:25 +0300

    jst-690 fix: install dotenv library, and edited firebase config with .gitignore

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -4,13 +4,13 @@ import { getFirestore } from 'firebase/firestore';
 import { getDatabase } from 'firebase/database';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyB-FlZ3sCzIKFKDSi3RD8imyEd-She8srw',
-  authDomain: 'clever-todo-list-51118.firebaseapp.com',
-  projectId: 'clever-todo-list-51118',
-  storageBucket: 'clever-todo-list-51118.appspot.com',
-  messagingSenderId: '611139966705',
-  appId: '1:611139966705:web:5977bdf89c7e334b0a3b50',
-  databaseURL: 'https://clever-todo-list-51118-default-rtdb.europe-west1.firebasedatabase.app',
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  databaseURL: process.env.DATABASE_URL,
 };
 
 export const app = initializeApp(firebaseConfig);
